bceef39d7dc7b1f3fd1652f94f6bd42c7b51019a
Rob Tillaart <rob.tillaart@gmail.com>
2024-03-06 21:48:00 +0100

    initial version

---
+++ examples/MAX521_demo/MAX521_demo.ino
@@ -0,0 +1,41 @@
+//
+//    FILE: MAX521_demo.ino
+//  AUTHOR: Rob Tillaart
+// PURPOSE: demo MAX521
+//     URL: https://github.com/RobTillaart/MAX520
+
+
+#include "MAX520.h"
+
+MAX521 mx(0x38);
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+  Serial.print("MAX520_LIB_VERSION:\t");
+  Serial.println(MAX520_LIB_VERSION);
+
+  Wire.begin();
+
+  mx.begin();
+
+}
+
+
+void loop()
+{
+  for (int ch = 0; ch < 8; ch++)
+  {
+    for (int value = 0; value < 256; value++)
+    {
+      mx.write(ch, value);
+      delay(100);
+    }
+  }
+}
+
+
+//  -- END OF FILE --
+
